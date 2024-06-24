b0a492303292c2122040c8c716292a83aee300cb
Partha <parthadev76@gmail.com>
2024-06-24 12:51:08 +0600

    Cart added

---
+++ resources/views/components/frontend/common/sticky-cart.blade.php
@@ -0,0 +1,375 @@
+<div class="fixed_product_sticky fixed top-[40%] z-20 bg-white drop-shadow right-0" id="openModalButton">
+    <div class="flex flex-col items-center justify-center">
+        <div class="fixed_product_sticky_icon p-2">
+            <i class="fa-solid text-theme fa-cart-shopping text-xl"></i>
+        </div>
+        <div class="fixed_product_sticky_price">
+            <p class="subTotal">
+            </p>
+        </div>
+        <div class="fixed_product_sticky_count bg-theme text-white px-3 py-1">
+            <p id="item_count">7 items</p>
+        </div>
+    </div>
+</div>
+
+
+<!-- Modal -->
+<div id="cartModel" class="fixed top-0 left-full inset-0 bg-gray-600 bg-opacity-50 items-center justify-center z-50">
+    <div id="cartSlide" class="bg-white shadow-lg py-2 w-full h-screen max-w-md absolute -right-[600px] duration-300 top-0">
+        <!-- Fixed Product Cart Header -->
+        <div class="flex justify-between items-center px-2">
+            <div class="icon flex flex-row gap-2 justify-center items-center">
+                <div class="fixed_product_sticky_icon">
+                    <i class="fa-solid text-theme fa-cart-shopping text-2xl"></i>
+                </div>
+                <div class="w-6 h-6 bg-theme rounded-full flex items-center justify-center text-white">
+                    <span>10</span>
+                </div>
+            </div>
+            <button id="closeModalButton" class="text-gray-600 hover:text-gray-900 bg-red-500 px-2 rounded-sm">
+                <i class="fa-solid fa-xmark text-xl text-white"></i>
+            </button>
+        </div>
+        <hr class="mt-2">
+        <!--/ Fixed Product Cart Header -->
+        <!-- Fixed Product Cart Body -->
+        <div class="fixed_product_cart_body h-[700px] overflow-y-scroll">
+            <div class="px-2 py-2">
+                <div class="flex flex-row gap-1 justify-between items-center">
+                    <div>
+                        <img src="https://production-qcoom-user.s3-ap-southeast-1.amazonaws.com/static_image/2023-08-14T12:04:34.269_1.jpg" width="80" height="80" alt="">
+                    </div>
+                    <div>
+                        <h3>Men's Double Pocket Solid Shirt- Fushia</h3>
+                    </div>
+                    <div>
+                        <a class="card_remove text-red-600">
+                            <i class="fa-solid fa-trash"></i>
+                        </a>
+                    </div>
+                </div>
+                <div class="flex flex-row gap-1 justify-between items-center">
+                    <div>
+                        <img src="https://production-qcoom-user.s3-ap-southeast-1.amazonaws.com/static_image/2023-08-14T12:04:34.269_1.jpg" width="80" height="80" alt="">
+                    </div>
+                    <div>
+                        <h3>Men's Double Pocket Solid Shirt- Fushia</h3>
+                    </div>
+                    <div>
+                        <a class="card_remove text-red-600">
+                            <i class="fa-solid fa-trash"></i>
+                        </a>
+                    </div>
+                </div>
+                <div class="flex flex-row gap-1 justify-between items-center">
+                    <div>
+                        <img src="https://production-qcoom-user.s3-ap-southeast-1.amazonaws.com/static_image/2023-08-14T12:04:34.269_1.jpg" width="80" height="80" alt="">
+                    </div>
+                    <div>
+                        <h3>Men's Double Pocket Solid Shirt- Fushia</h3>
+                    </div>
+                    <div>
+                        <a class="card_remove text-red-600">
+                            <i class="fa-solid fa-trash"></i>
+                        </a>
+                    </div>
+                </div>
+                <div class="flex flex-row gap-1 justify-between items-center">
+                    <div>
+                        <img src="https://production-qcoom-user.s3-ap-southeast-1.amazonaws.com/static_image/2023-08-14T12:04:34.269_1.jpg" width="80" height="80" alt="">
+                    </div>
+                    <div>
+                        <h3>Men's Double Pocket Solid Shirt- Fushia</h3>
+                    </div>
+                    <div>
+                        <a class="card_remove text-red-600">
+                            <i class="fa-solid fa-trash"></i>
+                        </a>
+                    </div>
+                </div>
+                <div class="flex flex-row gap-1 justify-between items-center">
+                    <div>
+                        <img src="https://production-qcoom-user.s3-ap-southeast-1.amazonaws.com/static_image/2023-08-14T12:04:34.269_1.jpg" width="80" height="80" alt="">
+                    </div>
+                    <div>
+                        <h3>Men's Double Pocket Solid Shirt- Fushia</h3>
+                    </div>
+                    <div>
+                        <a class="card_remove text-red-600">
+                            <i class="fa-solid fa-trash"></i>
+                        </a>
+                    </div>
+                </div>
+                <div class="flex flex-row gap-1 justify-between items-center">
+                    <div>
+                        <img src="https://production-qcoom-user.s3-ap-southeast-1.amazonaws.com/static_image/2023-08-14T12:04:34.269_1.jpg" width="80" height="80" alt="">
+                    </div>
+                    <div>
+                        <h3>Men's Double Pocket Solid Shirt- Fushia</h3>
+                    </div>
+                    <div>
+                        <a class="card_remove text-red-600">
+                            <i class="fa-solid fa-trash"></i>
+                        </a>
+                    </div>
+                </div>
+                <div class="flex flex-row gap-1 justify-between items-center">
+                    <div>
+                        <img src="https://production-qcoom-user.s3-ap-southeast-1.amazonaws.com/static_image/2023-08-14T12:04:34.269_1.jpg" width="80" height="80" alt="">
+                    </div>
+                    <div>
+                        <h3>Men's Double Pocket Solid Shirt- Fushia</h3>
+                    </div>
+                    <div>
+                        <a class="card_remove text-red-600">
+                            <i class="fa-solid fa-trash"></i>
+                        </a>
+                    </div>
+                </div>
+                <div class="flex flex-row gap-1 justify-between items-center">
+                    <div>
+                        <img src="https://production-qcoom-user.s3-ap-southeast-1.amazonaws.com/static_image/2023-08-14T12:04:34.269_1.jpg" width="80" height="80" alt="">
+                    </div>
+                    <div>
+                        <h3>Men's Double Pocket Solid Shirt- Fushia</h3>
+                    </div>
+                    <div>
+                        <a class="card_remove text-red-600">
+                            <i class="fa-solid fa-trash"></i>
+                        </a>
+                    </div>
+                </div>
+                <div class="flex flex-row gap-1 justify-between items-center">
+                    <div>
+                        <img src="https://production-qcoom-user.s3-ap-southeast-1.amazonaws.com/static_image/2023-08-14T12:04:34.269_1.jpg" width="80" height="80" alt="">
+                    </div>
+                    <div>
+                        <h3>Men's Double Pocket Solid Shirt- Fushia</h3>
+                    </div>
+                    <div>
+                        <a class="card_remove text-red-600">
+                            <i class="fa-solid fa-trash"></i>
+                        </a>
+                    </div>
+                </div>
+                <div class="flex flex-row gap-1 justify-between items-center">
+                    <div>
+                        <img src="https://production-qcoom-user.s3-ap-southeast-1.amazonaws.com/static_image/2023-08-14T12:04:34.269_1.jpg" width="80" height="80" alt="">
+                    </div>
+                    <div>
+                        <h3>Men's Double Pocket Solid Shirt- Fushia</h3>
+                    </div>
+                    <div>
+                        <a class="card_remove text-red-600">
+                            <i class="fa-solid fa-trash"></i>
+                        </a>
+                    </div>
+                </div>
+                <div class="flex flex-row gap-1 justify-between items-center">
+                    <div>
+                        <img src="https://production-qcoom-user.s3-ap-southeast-1.amazonaws.com/static_image/2023-08-14T12:04:34.269_1.jpg" width="80" height="80" alt="">
+                    </div>
+                    <div>
+                        <h3>Men's Double Pocket Solid Shirt- Fushia</h3>
+                    </div>
+                    <div>
+                        <a class="card_remove text-red-600">
+                            <i class="fa-solid fa-trash"></i>
+                        </a>
+                    </div>
+                </div>
+                <div class="flex flex-row gap-1 justify-between items-center">
+                    <div>
+                        <img src="https://production-qcoom-user.s3-ap-southeast-1.amazonaws.com/static_image/2023-08-14T12:04:34.269_1.jpg" width="80" height="80" alt="">
+                    </div>
+                    <div>
+                        <h3>Men's Double Pocket Solid Shirt- Fushia</h3>
+                    </div>
+                    <div>
+                        <a class="card_remove text-red-600">
+                            <i class="fa-solid fa-trash"></i>
+                        </a>
+                    </div>
+                </div><div class="flex flex-row gap-1 justify-between items-center">
+                    <div>
+                        <img src="https://production-qcoom-user.s3-ap-southeast-1.amazonaws.com/static_image/2023-08-14T12:04:34.269_1.jpg" width="80" height="80" alt="">
+                    </div>
+                    <div>
+                        <h3>Men's Double Pocket Solid Shirt- Fushia</h3>
+                    </div>
+                    <div>
+                        <a class="card_remove text-red-600">
+                            <i class="fa-solid fa-trash"></i>
+                        </a>
+                    </div>
+                </div>
+                <div class="flex flex-row gap-1 justify-between items-center">
+                    <div>
+                        <img src="https://production-qcoom-user.s3-ap-southeast-1.amazonaws.com/static_image/2023-08-14T12:04:34.269_1.jpg" width="80" height="80" alt="">
+                    </div>
+                    <div>
+                        <h3>Men's Double Pocket Solid Shirt- Fushia</h3>
+                    </div>
+                    <div>
+                        <a class="card_remove text-red-600">
+                            <i class="fa-solid fa-trash"></i>
+                        </a>
+                    </div>
+                </div>
+                <div class="flex flex-row gap-1 justify-between items-center">
+                    <div>
+                        <img src="https://production-qcoom-user.s3-ap-southeast-1.amazonaws.com/static_image/2023-08-14T12:04:34.269_1.jpg" width="80" height="80" alt="">
+                    </div>
+                    <div>
+                        <h3>Men's Double Pocket Solid Shirt- Fushia</h3>
+                    </div>
+                    <div>
+                        <a class="card_remove text-red-600">
+                            <i class="fa-solid fa-trash"></i>
+                        </a>
+                    </div>
+                </div>
+                <div class="flex flex-row gap-1 justify-between items-center">
+                    <div>
+                        <img src="https://production-qcoom-user.s3-ap-southeast-1.amazonaws.com/static_image/2023-08-14T12:04:34.269_1.jpg" width="80" height="80" alt="">
+                    </div>
+                    <div>
+                        <h3>Men's Double Pocket Solid Shirt- Fushia</h3>
+                    </div>
+                    <div>
+                        <a class="card_remove text-red-600">
+                            <i class="fa-solid fa-trash"></i>
+                        </a>
+                    </div>
+                </div>
+                <div class="flex flex-row gap-1 justify-between items-center">
+                    <div>
+                        <img src="https://production-qcoom-user.s3-ap-southeast-1.amazonaws.com/static_image/2023-08-14T12:04:34.269_1.jpg" width="80" height="80" alt="">
+                    </div>
+                    <div>
+                        <h3>Men's Double Pocket Solid Shirt- Fushia</h3>
+                    </div>
+                    <div>
+                        <a class="card_remove text-red-600">
+                            <i class="fa-solid fa-trash"></i>
+                        </a>
+                    </div>
+                </div>
+                <div class="flex flex-row gap-1 justify-between items-center">
+                    <div>
+                        <img src="https://production-qcoom-user.s3-ap-southeast-1.amazonaws.com/static_image/2023-08-14T12:04:34.269_1.jpg" width="80" height="80" alt="">
+                    </div>
+                    <div>
+                        <h3>Men's Double Pocket Solid Shirt- Fushia</h3>
+                    </div>
+                    <div>
+                        <a class="card_remove text-red-600">
+                            <i class="fa-solid fa-trash"></i>
+                        </a>
+                    </div>
+                </div>
+                <div class="flex flex-row gap-1 justify-between items-center">
+                    <div>
+                        <img src="https://production-qcoom-user.s3-ap-southeast-1.amazonaws.com/static_image/2023-08-14T12:04:34.269_1.jpg" width="80" height="80" alt="">
+                    </div>
+                    <div>
+                        <h3>Men's Double Pocket Solid Shirt- Fushia</h3>
+                    </div>
+                    <div>
+                        <a class="card_remove text-red-600">
+                            <i class="fa-solid fa-trash"></i>
+                        </a>
+                    </div>
+                </div>
+                <div class="flex flex-row gap-1 justify-between items-center">
+                    <div>
+                        <img src="https://production-qcoom-user.s3-ap-southeast-1.amazonaws.com/static_image/2023-08-14T12:04:34.269_1.jpg" width="80" height="80" alt="">
+                    </div>
+                    <div>
+                        <h3>Men's Double Pocket Solid Shirt- Fushia</h3>
+                    </div>
+                    <div>
+                        <a class="card_remove text-red-600">
+                            <i class="fa-solid fa-trash"></i>
+                        </a>
+                    </div>
+                </div>
+                <div class="flex flex-row gap-1 justify-between items-center">
+                    <div>
+                        <img src="https://production-qcoom-user.s3-ap-southeast-1.amazonaws.com/static_image/2023-08-14T12:04:34.269_1.jpg" width="80" height="80" alt="">
+                    </div>
+                    <div>
+                        <h3>Men's Double Pocket Solid Shirt- Fushia</h3>
+                    </div>
+                    <div>
+                        <a class="card_remove text-red-600">
+                            <i class="fa-solid fa-trash"></i>
+                        </a>
+                    </div>
+                </div>
+
+                <div class="flex flex-row gap-1 justify-between items-center">
+                    <div>
+                        <img src="https://production-qcoom-user.s3-ap-southeast-1.amazonaws.com/static_image/2023-08-14T12:04:34.269_1.jpg" width="80" height="80" alt="">
+                    </div>
+                    <div>
+                        <h3>Men's Double Pocket Solid Shirt- Fushia</h3>
+                    </div>
+                    <div>
+                        <a class="card_remove text-red-600">
+                            <i class="fa-solid fa-trash"></i>
+                        </a>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+        <!--/ Fixed Product Cart Body -->
+        <!-- Fixed Product Cart Footer -->
+        <div class="fixed_product_cart_footer bg-[#f5f6f7] absolute left-0 bottom-0 w-full py-4">
+            <div class="px-2 flex flex-col gap-2">
+                <div class="subtotal_container flex flex-row justify-between items-center">
+                    <span>Subtotal:</span>
+                    <span>342345 Tk</span>
+                </div>
+                <a href="" class="px-4 py-3 bg-theme font-bold text-white text-center">Checkout</a>
+                <a href="" class="px-4 py-3 font-bold bg-black text-white text-center">View Cart</a>
+            </div>
+        </div>
+        <!--/ Fixed Product Cart Footer -->
+    </div>
+</div>
+
+<script>
+    const openModalButton = document.getElementById('openModalButton');
+    const closeModalButton = document.getElementById('closeModalButton');
+    const modal = document.getElementById('cartModel');
+    const sliding = document.getElementById('cartSlide');
+
+    openModalButton.addEventListener('click', () => {
+        modal.classList.remove('left-full');
+        modal.classList.add('left-0');
+        document.body.classList.add("overflow-hidden");
+        sliding.classList.remove('-right-[600px]')
+        sliding.classList.add('-right-0')
+    });
+
+    closeModalButton.addEventListener('click', () => {
+        modal.classList.add('left-full');
+        modal.classList.remove('left-0');
+        document.body.classList.remove("overflow-hidden");
+        sliding.classList.remove('-right-0')
+        sliding.classList.add('-right-[600px]')
+    });
+
+    window.addEventListener('click', (e) => {
+        if (e.target === modal) {
+            modal.classList.add('left-full');
+            modal.classList.remove('left-0');
+            sliding.classList.remove('-right-0')
+            sliding.classList.add('-right-[600px]')
+            document.body.classList.remove("overflow-hidden");
+        }
+    });
+</script>
