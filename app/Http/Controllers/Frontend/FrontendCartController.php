432330462551f436b514c673b11cd8abcf1bc1d9
WebcoderPartha <parthadev76@gmail.com>
2024-07-17 14:19:55 +0600

    checkout and cart page fixed without any issue

--- app/Http/Controllers/Frontend/FrontendCartController.php
+++ app/Http/Controllers/Frontend/FrontendCartController.php
@@ -5,15 +5,25 @@ namespace App\Http\Controllers\Frontend;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\ShippingCharge;
+use Gloudemans\Shoppingcart\Facades\Cart;
 
 class FrontendCartController extends Controller
 {
     public function cart_view(){
-        return view('frontend.cart');
+       if (Cart::instance('shopping')->content()->count() > 0) {
+           return view('frontend.cart');
+       }else{
+           return redirect()->route('frontend.home_page');
+       }
     }
 
     public function checkout_view(){
-        $shipping_charge = ShippingCharge::all();
-        return view('frontend.checkout', compact('shipping_charge'));
+        if (Cart::instance('shopping')->content()->count() > 0){
+            $shipping_charge = ShippingCharge::all();
+            return view('frontend.checkout', compact('shipping_charge'));
+        }else {
+            return redirect()->route('frontend.home_page');
+        }
+
     }
 }
