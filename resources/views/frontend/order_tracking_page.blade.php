03b0ab21aed657baaf62db628353385c64932569
WebcoderPartha <parthadev76@gmail.com>
2024-09-05 12:38:25 +0600

    tracking page added

--- resources/views/frontend/order_tracking_page.blade.php
+++ resources/views/frontend/order_tracking_page.blade.php
@@ -6,30 +6,48 @@
             <div class="flex flex-col gap-2">
                 <h2>TRACK YOUR CONSIGNMENT</h2>
                 <div class="flex flex-row items-center">
-                    <input type="text" class="block w-full py-3 px-4 text-sm text-gray-900 border border-theme outline-none" placeholder="Enter order id">
-                    <button type="submit" class="bg-theme text-white px-4 py-[11px]">
+                    <input id="track_id" type="text" class="block w-full py-3 px-4 text-sm text-gray-900 border border-theme outline-none" placeholder="Enter order id">
+                    <button type="button" onclick="trackOrder()" class="bg-theme text-white px-4 py-[11px]">
                         <i class="fa-solid fa-magnifying-glass"></i>
                     </button>
                 </div>
-                <div class="mt-4 border-t border-gray-300">
-                    <div class="bg-gray-300 py-2 text-center uppercase font-semibold mb-2">
-                        Track Information
-                    </div>
-                    <div class="flex item items-center justify-between">
-                        <div class="flex flex-col gap-1">
-                            <p>2024/Aug/14 17:08 pm</p>
-                            <p><b>Invoice No :</b> 1944</p>
-                            <p><b>Address :</b> 93 Kazi Nazrul Islam Avenue, Northern<br> University Building (5th Floor), Kawran Bazar, Dhaka-1215</p>
-                            <p><b>Mobile :</b> 01710147887</p>
-                        </div>
-                        <div class="flex flex-col gap-2">
-                            <h2>Online: 420 Tk</h2>
-                            <div class="bg-theme rounded-2xl text-white px-2 py-1 text-center">Shipment</div>
-                        </div>
-                    </div>
+                <div class="mt-4 border-t border-gray-300" id="order_track">
+
                 </div>
             </div>
         </div>
+
+        <script>
+            function trackOrder(){
+               let track_id = document.getElementById('track_id').value;
+               if (track_id.length > 0){
+
+                   $('#order_track').html(`
+                    <div>
+                        <div class="bg-gray-300 py-2 text-center uppercase font-semibold mb-2">
+                            Track Information
+                        </div>
+                        <div class="flex item items-center justify-between">
+                            <div class="flex flex-col gap-1">
+                                <p>2024/Aug/14 17:08 pm</p>
+                                <p><b>Invoice No :</b> 1944</p>
+                                <p><b>Address :</b> 93 Kazi Nazrul Islam Avenue, Northern<br> University Building (5th Floor), Kawran Bazar, Dhaka-1215</p>
+                                <p><b>Mobile :</b> 01710147887</p>
+                            </div>
+                            <div class="flex flex-col gap-2">
+                                <h2>Online: 420 Tk</h2>
+                                <div class="bg-theme rounded-2xl text-white px-2 py-1 text-center">Shipment</div>
+                            </div>
+                        </div>
+                    </div>
+                   `)
+
+               }else{
+                   toastr.error('Please enter order id');
+               }
+
+            }
+        </script>
 
 @endsection
 
